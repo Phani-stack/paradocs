de279e01043ee3f86c36fcdd5dffa2fd5ab3ef2c
Phani-stack <kdlphani916@gmail.com>
2026-04-09 22:59:13 +0530

    h

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://paradocs.onrender.com/api",
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://paradocs.onrender.com/api",
+  // baseURL: "http://localhost:8000/api",
 });
 
 // Auth
